60d96445f751157c122f1f0033b25774fddcce53
rlawndks4 <rlawndks0423@naver.com>
2023-11-06 18:50:03 +0900

    feaT: udpate

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -138,6 +138,10 @@ const userCtrl = {
                 profile_img,
                 brand_id, user_name, name, nickname, level, phone_num, note, id
             } = req.body;
+            let is_exist_user = await pool.query(`SELECT * FROM ${table_name} WHERE user_name=? AND brand_id=${brand_id} AND id!=?`, [user_name, id]);
+            if (is_exist_user?.result.length > 0) {
+                return response(req, res, -100, "유저아이디가 이미 존재합니다.", false)
+            }
             let files = settingFiles(req.files);
             let obj = {
                 profile_img,
