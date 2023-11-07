83d6d1e50fb4893c4148300459af89980e2fb53b
rlawndks4 <rlawndks0423@naver.com>
2023-11-07 16:35:38 +0900

    feat: update

--- controllers/shop.controller.js
+++ controllers/shop.controller.js
@@ -49,21 +49,21 @@ const shopCtrl = {
             product_sql += ` WHERE products.id IN(${product_ids.join()}) `;
             product_sql += ` AND products.is_delete=0 `
             product_sql = product_sql.replaceAll(process.env.SELECT_COLUMN_SECRET, product_columns.join());
-            //product categories
+            //상품카테고리그룹
             let product_category_group_columns = [
                 `product_category_groups.*`,
             ]
             let product_category_group_sql = `SELECT ${product_category_group_columns.join()} FROM product_category_groups `;
             product_category_group_sql += ` WHERE product_category_groups.brand_id=${decode_dns?.id} `;
             product_category_group_sql += ` AND product_category_groups.is_delete=0 ORDER BY sort_idx DESC`;
-
+            //상품카테고리  
             let product_category_columns = [
                 `product_categories.*`,
             ]
             let product_category_sql = `SELECT ${product_category_columns.join()} FROM product_categories `;
             product_category_sql += ` WHERE product_categories.brand_id=${decode_dns?.id} `;
             product_category_sql += ` AND product_categories.is_delete=0 ORDER BY sort_idx DESC`;
-
+            //상품리뷰     
             let product_review_columns = [
                 `product_reviews.*`,
             ]
@@ -71,14 +71,14 @@ const shopCtrl = {
             product_review_sql += ` WHERE product_reviews.brand_id=${decode_dns?.id} `;
             product_review_sql += ` AND product_reviews.is_delete=0 ORDER BY sort_idx DESC`;
 
-
+            //게시물카테고리
             let post_category_columns = [
                 `post_categories.*`,
             ]
             let post_category_sql = `SELECT ${post_category_columns.join()} FROM post_categories `;
             post_category_sql += ` WHERE post_categories.brand_id=${decode_dns?.id} `;
             post_category_sql += ` AND post_categories.is_delete=0 ORDER BY sort_idx DESC`;
-
+            //셀러
             let seller_columns = [
                 `users.*`,
             ]
@@ -87,14 +87,14 @@ const shopCtrl = {
             seller_sql += ` AND level=10 `;
             seller_sql += ` AND is_delete=0 `;
             seller_sql += ` ORDER BY id DESC`;
-
+            //결제모듈
             let payment_module_columns = [
                 `payment_modules.*`,
             ]
             let payment_module_sql = `SELECT ${payment_module_columns.join()} FROM payment_modules `;
             payment_module_sql += ` WHERE payment_modules.brand_id=${decode_dns?.id} `;
             payment_module_sql += ` ORDER BY id DESC`;
-
+            //유저찜
             let user_wish_columns = [
                 `user_wishs.*`,
 
@@ -142,7 +142,7 @@ const shopCtrl = {
                 return item?.id
             })
             post_category_ids.unshift(0);
-            let recent_post_sql = `SELECT id, category_id, post_title FROM posts WHERE category_id IN (${post_category_ids.join()}) GROUP BY category_id, id HAVING COUNT(*) <= 10`;
+            let recent_post_sql = `SELECT id, category_id, post_title FROM posts WHERE category_id IN (${post_category_ids.join()}) AND is_delete=0 GROUP BY category_id, id HAVING COUNT(*) <= 10`;
             let recent_post_data = await pool.query(recent_post_sql)
             recent_post_data = recent_post_data?.result;
             for (var i = 0; i < data?.post_categories.length; i++) {
