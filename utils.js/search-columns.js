5f1f8a3f9febf49b9428f7326e73af90d36ed00b
rladlsdnr <kimin6756@gmail.com>
2024-02-07 04:00:08 +0900

    test

--- utils.js/search-columns.js
+++ utils.js/search-columns.js
@@ -1,5 +1,5 @@
 
 export const searchColumns = {
-    'products': ['product_name', 'product_comment'],
+    'products': ['product_name', 'product_comment',],
     'transactions': ['buyer_name', 'appr_num'],
 }
