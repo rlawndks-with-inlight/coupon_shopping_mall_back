06aab1035d6669156a13c9c6ec42216864fbf41b
rlawndks4 <rlawndks0423@naver.com>
2024-02-09 03:49:29 +0900

    update

--- utils.js/schedules/index.js
+++ utils.js/schedules/index.js
@@ -1,12 +1,16 @@
 import schedule from "node-schedule";
 import { returnMoment } from "../function.js";
 import { langProcess } from "./lang-process.js";
+import { setGrandParisProducts } from "../../controllers/util.controller.js";
 
 const scheduleIndex = () => {
   schedule.scheduleJob("0 0/1 * * * *", async function () {
     let return_moment = returnMoment();
 
     langProcess();
+    if (return_moment.includes('00:00:')) {
+      setGrandParisProducts();
+    }
   });
 };
 
