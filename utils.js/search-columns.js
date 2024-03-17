a2ef43979918cccee0a10a3eb10e1a2aa829a240
rlawndks4 <rlawndks0423@naver.com>
2024-03-17 21:17:48 +0900

    update

--- utils.js/search-columns.js
+++ utils.js/search-columns.js
@@ -1,6 +1,6 @@
 
 export const searchColumns = {
-    'products': ['product_name', 'product_comment', 'product_code', `product_categories_0.category_name`, `product_categories_1.category_en_name`],
+    'products': ['product_name', 'product_comment', 'product_code', 'lang_obj', `product_categories_0.category_name`, `product_categories_1.category_en_name`],
     'transactions': ['buyer_name', 'appr_num'],
     'users': ['user_name', 'name', 'nickname', 'phone_num']
 }
