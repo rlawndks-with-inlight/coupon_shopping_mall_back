c2b5c99f835f5384727024f353fd3913138cfe20
rladlsdnr <kimin6756@gmail.com>
2024-03-26 01:35:53 +0900

    검색어 설정

--- utils.js/search-columns.js
+++ utils.js/search-columns.js
@@ -1,6 +1,6 @@
 
 export const searchColumns = {
-    'products': ['product_name', 'product_comment', 'product_code', 'products.lang_obj', `product_categories_0.category_name`, `product_categories_1.category_en_name`],
+    'products': ['product_name', 'product_comment', 'product_code', 'products.lang_obj', `product_categories_0.category_name`, `product_categories_1.category_en_name`, `consignment_none_user_name`, `consignment_users.user_name`, `consignment_users.name`, `consignment_users.nickname`, `consignment_users.user_name`,],
     'transactions': ['buyer_name', 'appr_num'],
     'users': ['user_name', 'name', 'nickname', 'phone_num']
 }
