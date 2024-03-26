585ed55e083f9d72d2fe72456d0b9c9dc1f8567c
rladlsdnr <kimin6756@gmail.com>
2024-03-26 17:22:10 +0900

    스케줄러

--- utils.js/schedules/index.js
+++ utils.js/schedules/index.js
@@ -2,6 +2,7 @@ import schedule from "node-schedule";
 import { returnMoment } from "../function.js";
 import { langProcess } from "./lang-process.js";
 import { setGrandParisProducts } from "../../controllers/util.controller.js";
+import getArfighterItems from "../corps/arfighter.js";
 
 const scheduleIndex = () => {
   schedule.scheduleJob("0 0/1 * * * *", async function () {
@@ -10,6 +11,9 @@ const scheduleIndex = () => {
     langProcess();
     if (return_moment.includes('00:00:')) {
       setGrandParisProducts();
+    }
+    if (return_moment.includes('00:00:')) {
+      getArfighterItems();
     }
   });
 };
