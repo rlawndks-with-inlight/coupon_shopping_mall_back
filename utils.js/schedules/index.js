8a51d72324915e0ade89a89d9e6b68473bb023e2
rladlsdnr <kimin6756@gmail.com>
2024-07-05 23:21:58 +0900

    demo7

--- utils.js/schedules/index.js
+++ utils.js/schedules/index.js
@@ -6,6 +6,9 @@ import { getArfighterItems } from '../corps/arfighter.js'
 
 const scheduleIndex = () => {
   schedule.scheduleJob("0 0/1 * * * *", async function () {
+    if (parseInt(process.env.INSTANCE_ID) != parseInt(process.env.instances) - 1) {
+      return;
+    }
     let return_moment = returnMoment();
     langProcess();
     if (return_moment.includes('00:00:')) {
