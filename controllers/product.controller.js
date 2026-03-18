2d81564432f05f0ad5980bf263a9d903f53c9365
rladlsdnr <kimin6756@naver.com>
2026-03-18 23:56:37 +0900

    0318

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -305,7 +305,6 @@ const productCtrl = {
             /*if (!decode_user || decode_user?.level < 10) {
                 sql += ` AND products.status!=5 `
             }*/
-            console.log(`[product-search] req.query=`, JSON.stringify(req.query));
             let data = await getSelectQueryList(sql, columns, { ...req.query, type: type }, [], params);
             let product_ids = data?.content.map(item => { return item?.id });
             product_ids.unshift(0);
